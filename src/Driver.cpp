61a0920a3c98e76f88b642412f6e4d43e1ecb77f
Expodecay <rick.ramirez6174@gmail.com>
2023-10-08 23:26:24 -0700

    adding driver

---
+++ src/Driver.cpp
@@ -0,0 +1,16 @@
+#include <QApplication>
+
+#include <mainwindow.h>
+
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+	QApplication app(argc, argv);
+
+	MainWindow* window = new MainWindow();
+
+	window->show();
+	return app.exec();
+}
